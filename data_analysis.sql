8b05138e0bef75c23e4b296fb75e83dd87fce7d0
Jaison T Rajan <130531622+jaison-rajan@users.noreply.github.com>
2024-01-26 23:09:07 +0530

    data_analysis.sql

--- data_analysis.sql
+++ data_analysis.sql
@@ -10,7 +10,7 @@ FROM
 GROUP BY 
   member_casual;
 
--- Of the 873,924 rides, 255589 were casual users and 618335 pay for annual membership 
+-- Of the 5.38 million rides, 62 %  were annual subscribers and 38 % were casual riders
 
 --What type of bikes do casual users prefer?
 
@@ -26,7 +26,7 @@ GROUP BY
 ORDER BY 
   numb_of_rides DESC;
 
---Casual users most often use electric bikes.
+--Casual users most often use docked bikes.
 
 --What type of bikes do annual subscribers prefer?
 SELECT
@@ -56,24 +56,7 @@ GROUP BY
 ORDER BY 
   numb_of_rides DESC;
 
---Casual riders use Cyclistic most often on Saturdays, followed by Sunday and Monday. The least popular day is Friday
-
---On what day of the week do casual users have the longest ride times?
-
-SELECT
-  day_of_week,
-  COUNT (day_of_week) AS numb_of_rides,
-  AVG(ride_length) AS avg_ride_length
-FROM 
-  quick-discovery-402518.cyclistic_project.DataForAnalysis
-WHERE 
-  member_casual = 'casual'
-GROUP BY 
-  day_of_week
-ORDER BY 
-  avg_ride_length DESC;
-
---Casual riders have the longest average ride time on Saturday, followed by Sunday and Mondays. The shortest ride times take place on Friday.
+--Casual riders use Cyclistic most often on Saturdays, followed by Sunday. The least popular day is Monday.
 
 
 --On what day of the week do annual subscribers most use Cyclistic?
@@ -90,40 +73,10 @@ GROUP BY
 ORDER BY
    numb_of_rides DESC;
 
---Annual subscribers most often use Cyclistic on Tuesday, followed by thursday and Wednesday. The least popular day is Sunday.
+--Annual subscribers most often use Cyclistic on Wednesday, Thursday and Tuesday.. The least popular day is Sunday.
 
 
---On what day of the week do annual subscribers have the longest ride times?
-SELECT
-  day_of_week,
-  COUNT (day_of_week) AS numb_of_rides,
-  AVG(ride_length) AS avg_ride_length
-FROM 
-  quick-discovery-402518.cyclistic_project.DataForAnalysis
-WHERE 
-  member_casual = 'member'
-GROUP BY 
-  day_of_week
-ORDER BY 
-  avg_ride_length DESC;
-
---Annual subscribers have the longest ride times on Saturday, followed by Sunday and Monday. The shortest rides take place on Tuesday
-
---What time of year is the most popular for all Cyclistic members?
-SELECT
-  month, 
-  COUNT(month) AS numb_of_rides,
-  AVG(ride_length) AS avg_ride_length
-FROM 
-  quick-discovery-402518.cyclistic_project.DataForAnalysis
-GROUP BY 
-  month
-ORDER BY 
-  numb_of_rides DESC;
-
---Across all member types, the most popular month is April. The least popular months is January
-
---Does this vary between casual users and annual subscribers?
+--What time of year is the most popular for casual users and annual subscribers?
 SELECT
   month, 
   COUNT(month) AS numb_of_rides,
@@ -150,7 +103,7 @@ GROUP BY
 ORDER BY 
   numb_of_rides DESC;
 
---Both prefer most in April and least in Janaury
+--Both prefer most in July. But when it comes to leat popularity, its January for Casual Riders and December for Annual Subscribers.
 
 --At what time of year do the longest rides take place?
 SELECT
@@ -164,7 +117,7 @@ GROUP BY
 ORDER BY 
   avg_ride_length DESC;
 
---Across all member types, the longest rides take place in March. The shortest average rides take place in Feburuary 
+--Across all member types, the longest rides take place in July. 
 
 --Does this vary between casual members and annual subscribers?
 --At what time of year do the longest rides take place?
@@ -181,7 +134,7 @@ GROUP BY
 ORDER BY 
   avg_ride_length DESC;
 
---Casual members have the longest rides in March. They have the shortest rides in February
+--Casual members have the longest rides in July. They have the shortest rides in December.
 
 SELECT
   month, 
@@ -196,4 +149,4 @@ GROUP BY
 ORDER BY 
   avg_ride_length DESC
 
---Annual subscribers have the longest ride length in March. They have the shortest rides in  February.
+--Annual subscribers have the longest ride length in July. They have the shortest rides in  March and January.
