d9e2d3d1c5e17fb36b7ac6d06677df8d9fe6f75b
Jaison T Rajan <130531622+jaison-rajan@users.noreply.github.com>
2024-01-26 22:33:50 +0530

    data combination.sql

--- data combination.sql
+++ data combination.sql
@@ -1,11 +1,28 @@
---Create 2022Q1Data Table by combining data from January to April
+--Create 2022Q1Data Table by combining data from July 2022 to June 2023
 
-CREATE TABLE IF NOT EXISTS cyclistic_project.2022Q1Data 
+CREATE TABLE IF NOT EXISTS cyclistic_project.2022_2023Data 
 AS
-SELECT * FROM quick-discovery-402518.cyclistic_project.JanData
+SELECT * FROM quick-discovery-402518.cyclistic_project.Jul2022
 UNION ALL
-SELECT * FROM quick-discovery-402518.cyclistic_project.FebData
+SELECT * FROM quick-discovery-402518.cyclistic_project.Aug2022
 UNION ALL
-SELECT * FROM quick-discovery-402518.cyclistic_project.MarData
+SELECT * FROM quick-discovery-402518.cyclistic_project.Sep2022
 UNION ALL
-SELECT * FROM quick-discovery-402518.cyclistic_project.AprData
+SELECT * FROM quick-discovery-402518.cyclistic_project.Oct2022
+UNION ALL
+SELECT * FROM quick-discovery-402518.cyclistic_project.Nov2022
+UNION ALL
+SELECT * FROM quick-discovery-402518.cyclistic_project.Dec2022
+UNION ALL
+SELECT * FROM quick-discovery-402518.cyclistic_project.Jan2023
+UNION ALL
+SELECT * FROM quick-discovery-402518.cyclistic_project.Feb2023
+UNION ALL
+SELECT * FROM quick-discovery-402518.cyclistic_project.Mar2023
+UNION ALL
+SELECT * FROM quick-discovery-402518.cyclistic_project.Apr2023
+UNION ALL
+SELECT * FROM quick-discovery-402518.cyclistic_project.May2023
+UNION ALL
+SELECT * FROM quick-discovery-402518.cyclistic_project.Jun2023
+
